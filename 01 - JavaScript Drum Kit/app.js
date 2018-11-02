b6f10e122e0553d6d991dad3088c7d2be8a48508
Hannah <hannah.franky.may@gmail.com>
2018-11-01 19:58:48 -0500

    Did my first javascript 30 challenge

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,24 @@
+//Select each div square
+//add an event listener to each keyboad key with a corresponding sound and div square
+//add classes when keys are pressed
+
+
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
